ee1369c28bd63b3de17798beff718b5cbca2964b
Anthony Zavadil <anthony.zavadil@gmail.com>
2014-10-09 11:30:19 -0700

    Modify dump_env.py to run the function

--- dump_env.py
+++ dump_env.py
@@ -8,12 +8,12 @@ def dump_env():
   """
 
   f = open('envdump.txt', 'w')
-  port = os.environ['MONGODB_PORT']
-  port_27017 = os.environ['MONGODB_PORT_27017_TCP']
+  port = os.environ.get('MONGODB_PORT')
+  port_27017 = os.environ.get('MONGODB_PORT_27017_TCP')
 
   f.write("port: {0} port_27017 {1}".format(port, port_27017))
   f.close()
 
 
-
-  
+if __name__ == "__main__":
+  dump_env()
