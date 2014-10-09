ed3413c922b6bbd16c8b0421fb573c642b3c4dd4
Anthony Zavadil <anthony.zavadil@gmail.com>
2014-10-09 13:55:23 -0700

    Correct env_dump.py

--- dump_env.py
+++ dump_env.py
@@ -12,7 +12,7 @@ def dump_env():
   port_27017 = os.environ.get('MONGODB_PORT_27017_TCP')
   address = os.environ.get('MONGODB_PORT_27017_TCP_ADDR')
   tcp_port = os.environ.get('MONGODB_PORT_27017_TCP_PORT')
-  f.write(" MONGODB_PORT: {0}\n MONGODB_PORT_27017_TCP {1}\n MONGODB_PORT_27017_TCP_ADDR {2}\n MONGODB_PORT_27017_TCP_PORT".format(port, port_27017, address, tcp_port))
+  f.write(" MONGODB_PORT: {0}\n MONGODB_PORT_27017_TCP {1}\n MONGODB_PORT_27017_TCP_ADDR {2}\n MONGODB_PORT_27017_TCP_PORT {3}".format(port, port_27017, address, tcp_port))
   f.close()
 
 
